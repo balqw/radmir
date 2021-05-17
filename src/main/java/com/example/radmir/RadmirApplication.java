a86bd9cfe7d1b9060b1b9c0f82ba828f8fe4b437
balqw <balqw777@gmail.com>
2021-05-17 21:09:57 +0300

    init project

---
+++ src/main/java/com/example/radmir/RadmirApplication.java
@@ -0,0 +1,13 @@
+package com.example.radmir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadmirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadmirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/radmir/RadmirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.radmir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadmirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
