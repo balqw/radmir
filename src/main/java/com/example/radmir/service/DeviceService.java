717b3c31494bdf155a47da1b20eed60ec6bd4908
balqw <balqw777@gmail.com>
2021-05-21 08:15:36 +0300

    hotfix

--- src/main/java/com/example/radmir/service/DeviceService.java
+++ src/main/java/com/example/radmir/service/DeviceService.java
@@ -4,6 +4,9 @@ import com.example.radmir.model.Device;
 import com.example.radmir.repository.DeviceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,29 +20,30 @@ public class DeviceService {
     @Autowired
     private  ConfigProperties properties;
 
-
+    @Transactional
     public Device getDeviceById(Long id){
         return deviceRepository.findById(id).orElseThrow(()->new IllegalArgumentException(format("no device found with serial =%d",id)));
     }
 
-
+    @Transactional
     public List<Device>getDevices(){
         return deviceRepository.findAll();
     }
-
+    @Transactional
     public Device editDeviceById(Device device){
         return deviceRepository.save(device);
     }
-
+    @Transactional
     public Device saveDevice(Device device){
         return deviceRepository.save(device);
     }
-
+    @Transactional
     public void deleteDeviceById(Long id){
        deviceRepository.deleteById(id);
     }
-
+    @Transactional
     public List<Device>getBrokenDevices(){
+
         LocalDateTime time = LocalDateTime.now().minus(properties.getThresholdHours());
         return deviceRepository.findDevicesByLastResponseLessThanOrVolumeMeasuresLessThan(time, properties.getThresholdMeasures());
     }
